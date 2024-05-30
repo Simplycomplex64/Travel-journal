572241adfa5364b55e4f2d1ea2a7c5104a832452
simplyComplex <91500514+Simplycomplex64@users.noreply.github.com>
2024-05-30 19:36:23 -0400

    second commit

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 
 export default function Cards(props) {
-    console.log(props)
     return (
             <div className="cards-container">
                 <div className="card">
                 <img className="cards-img" src={`../public/images/${props.item.imageUrl}`} alt="" />
                 <div className="card-text-location">
                     <img src="../public/images/location-tag.png" alt="location tag" />
-                    <h2>{props.item.title}</h2>
+                    <div className="title-control">
+                        <h2>{props.item.title}</h2>
+                    </div>
                     <a href={`${props.item.googleMapsUrl}`}>View on Google Maps</a>
                 </div>
                 <div className="date-n-description">
                     <h4>From : <span>{`${props.item.startDate}`}</span> To : <span>{`${props.item.endDate}`}</span></h4>
-                        <p>
-                        Known as the 'City of Canals' there are many things Venice is famous for including its 
-                        beautiful bridges, gondola rides, atmospheric streets and carnival celebrations. Built 
-                        over 118 islands, Venice and its lagoon is one of the most unique cities in the world.
-                        </p>
+                    <div className="description">
+                    <p>{`${props.item.description}`}</p>
+                    </div>
                 </div>
                 </div>
             </div>
