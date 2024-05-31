90503017c6c559eb6121453ea8d3eeae50454794
simplyComplex <91500514+Simplycomplex64@users.noreply.github.com>
2024-05-30 20:36:46 -0400

    one more, more

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Cards(props) {
     return (
-            <div className="cards-container">
-                <div className="card">
-                <img className="cards-img" src={`../public/image/${props.item.imageUrl}`} alt="" />
+        <div className="cards-container">
+            <div className="card">
+                <img className="cards-img" src={`/${props.item.imageUrl}`} alt="" />
                 <div className="card-text-location">
-                    <img src="../public/image/location-tag.png" alt="location tag" />
+                    <img src="/location-tag.png" alt="location tag" />
                     <div className="title-control">
                         <h2>{props.item.title}</h2>
                     </div>
@@ -15,10 +15,10 @@ export default function Cards(props) {
                 <div className="date-n-description">
                     <h4>From : <span>{`${props.item.startDate}`}</span> To : <span>{`${props.item.endDate}`}</span></h4>
                     <div className="description">
-                    <p>{`${props.item.description}`}</p>
+                        <p>{`${props.item.description}`}</p>
                     </div>
                 </div>
-                </div>
             </div>
+        </div>
     );
 }
